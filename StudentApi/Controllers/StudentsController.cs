706b6d753fc03d4df15f25509ba78f16617816c5
Tejas Chitroda <tejaskumar.chitroda@simformsolutions.com>
2026-04-15 00:32:25 +0530

    feature/TASK-123:pre-commit checked

--- StudentApi/Controllers/StudentsController.cs
+++ StudentApi/Controllers/StudentsController.cs
@@ -19,10 +19,9 @@ public class StudentsController : ControllerBase
 
     // GET: api/students
     [HttpGet]
-    public async Task<ActionResult   <IEnumerable   <Student>>>      GetAllStudents()
+    public async Task<ActionResult<IEnumerable<Student>>> GetAllStudents()
     {
         _logger.LogInformation("Getting all students");
-        Console.WriteLine("Getting all students"); 
         var students = await _repository.GetAllAsync();
         return Ok(students);
     }
